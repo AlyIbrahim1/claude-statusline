bb6e0a7dc2d3fc855b7369ae7300124918ff6d6f
Aly Lotfy <AlyIbrahim1@users.noreply.github.com>
2026-03-25 20:40:49 +0200

    chore: add Rust crate skeleton

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use serde_json::Value;
+use std::io::Read;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let mut input = String::new();
+        std::io::stdin().read_to_string(&mut input).ok();
+        tx.send(input).ok();
+    });
+    let input = match rx.recv_timeout(Duration::from_secs(3)) {
+        Ok(s) => s,
+        Err(_) => return,
+    };
+    if let Some(out) = render(&input) {
+        print!("{}", out);
+    }
+}
+
+fn render(_input: &str) -> Option<String> {
+    Some(String::from("placeholder"))
+}
